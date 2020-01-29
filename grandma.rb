059cd1d26a0491800dee2c3f5075886a3d60214c
Willis Aybar <willisaybar@gmail.com>
2020-01-29 00:32:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,13 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma(string)
+  if string == string.downcase
+    return "HUH?! SPEAK UP, SONNY!".upcase
+    elsif string == string.upcase
+    return "NO, NOT SINCE 1938!".upcase
+    elsif string == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!".upcase
+  end
+end
